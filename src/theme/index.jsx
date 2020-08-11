70c92b47051400a15b46442603c14461d8222bd8
yohni <yohni.123@gmail.com>
2020-08-12 00:06:51 +0700

    Setup theme such as palletes, fontsizes, boxes, etc

---
+++ src/theme/index.jsx
@@ -0,0 +1,38 @@
+const Palletes = {
+  PinkPrimary: "#f9423a",
+  BrownChoco: "#a23530",
+  GreyDarken: "#424749",
+  GreyShadow: "#6e7679",
+  GreySmooth: "#e2e4e4",
+  WhiteClean: "#f1f1f2",
+};
+
+const FontSizes = {
+  xs: "8px",
+  sm: "12px",
+  md: "14px",
+  lg: "16px",
+  xl: "20px",
+};
+
+const Boxes = {
+  xs: "4px",
+  sm: "8px",
+  md: "16px",
+  lg: "24px",
+  xl: "32px",
+  xxl: "40px",
+};
+
+const Shadows = {
+  boxShadows: "0 8px 10px 0 rgba(10, 31, 68, 0.1)",
+};
+
+const Themes = {
+  Palletes,
+  FontSizes,
+  Boxes,
+  Shadows,
+};
+
+export default Themes;
