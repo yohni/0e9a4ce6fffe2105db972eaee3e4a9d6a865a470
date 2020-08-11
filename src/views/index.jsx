d211dd0ee101632812b9a18387803833b164a50c
yohni <yohni.123@gmail.com>
2020-08-12 00:07:05 +0700

    Init new homepage

---
+++ src/views/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainContainer from "../layout/MainContainer";
+import DatePicker from "../components/DatePicker";
+
+const HomePage = () => {
+  return (
+    <MainContainer>
+      <DatePicker />
+    </MainContainer>
+  );
+};
+
+export default HomePage;
