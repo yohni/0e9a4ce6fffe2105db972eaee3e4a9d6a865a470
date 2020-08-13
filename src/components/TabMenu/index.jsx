6cb75f67741d1ec9da8f57c6f34e276dd2b658a6
yohni <yohni.123@gmail.com>
2020-08-13 16:00:38 +0700

    Fixing responsibility of fixed position

--- src/components/TabMenu/index.jsx
+++ src/components/TabMenu/index.jsx
@@ -3,20 +3,22 @@ import styled, { ThemeProvider } from "styled-components";
 import Themes from "../../theme";
 
 const Wrapper = styled.div`
-  padding: ${(props) => props.theme.Boxes.sm};
+  padding: ${(props) => props.theme.Boxes.sm} ${(props) => props.theme.Boxes.md};
   background: #ffffff;
   position: fixed;
-  top: 104px;
+  top: 112 px;
   width: 100%;
-  max-width: 752px;
+  max-width: 736px;
+  z-index: 19;
 
   @media (max-width: 768px) {
-    max-width: calc(100vw - 16px);
+    max-width: calc(100vw - 32px);
   }
 `;
 
 const Container = styled.div`
   border-radius: 5px;
+  border: ${(props) => props.theme.Palletes.WhiteClean} solid 1px;
   display: flex;
   overflow: hidden;
 `;
@@ -54,7 +56,11 @@ const TabMenu = ({ tabItem, activeTab, showTab }) => {
         <Wrapper className={showTab ? "active" : "hidden"}>
           <Container>
             {tabItem.map((item) => (
-              <TabItem key={item.id} actived={item.id === activeTab}>
+              <TabItem
+                key={item.id}
+                actived={item.name === activeTab}
+                onClick={item.func}
+              >
                 {item.name}
               </TabItem>
             ))}
